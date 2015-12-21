3c4b2a48a076551f4bf900e8b6dde84309eb8693
cgf1993 <767954870@qq.com>
2015-12-21 22:37:07 +0800

    modify test.java

--- test.java
+++ test.java
@@ -4,5 +4,8 @@ public class test {
     public static void main(String[] args) {
         System.out.println("what a shit");
     }
+    public void haha() {
+        System.out.println("nothin");
+    }
 }
 
