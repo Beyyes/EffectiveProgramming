2162af50b37ad4a21a02635c8c412b0d00d8a7d8
GF.Cao <767954870@qq.com>
2017-03-20 23:52:24 +0800

    Update Basis_Usage.scala

--- Scala/Scala_Basic/Basis_Usage.scala
+++ Scala/Scala_Basic/Basis_Usage.scala
@@ -13,12 +13,25 @@ private[basis] object Basis_Usage {
     //      case _ => print("default")
     //    }
 
+
     // Option Usage
-    // Option[Int]
-
-    val line = "xx"
-    val lineAnnotation = line.map(l => s" line $l").getOrElse("")
-
+    // Scala Option(选项)类型用来表示一个值是可选的（有值或无值)。
+    // Option[T] 是一个类型为 T 的可选值的容器： 如果值存在， Option[T] 就是一个 Some[T] ，如果不存在， Option[T] 就是对象 None 。
+    val myMap: Map[String, String] = Map("key1" -> "value")
+    val value1: Option[String] = myMap.get("key1")
+    val value2: Option[String] = myMap.get("key2")
+    println(value1) // Some("value1")
+    println(value2) // None
+    // 你可以使用 getOrElse() 方法来获取元组中存在的元素或者使用其默认的值，实例如下：
+    val a:Option[Int] = Some(5)
+    val b:Option[Int] = None
+    println("a.getOrElse(0): " + a.getOrElse(0) )
+    println("b.getOrElse(10): " + b.getOrElse(0) )
+    val lineAnnotation = a.map(l => s" line $l").getOrElse("") //getOrElse使用默认值或存在值
+    println(lineAnnotation)
+    
+    
+    // Other Usage
   }
 
   def haha() = print("haha")
